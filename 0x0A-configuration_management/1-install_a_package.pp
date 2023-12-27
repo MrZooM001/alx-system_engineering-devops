bfbf995b6b7c1f79c8752e0f88f269b13b588423
Hazem Magdy <hhazem0010@gmail.com>
2023-12-27 13:30:23 -0800

    fix 5

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,20 @@
 # install flask from pip3, Using Puppet
 #!/usr/bin/pup
 
+# install python3 from pip3
 package { 'python3':
   ensure   => '3.8.10',
   provider => 'pip3'
 }
 
+# install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
 
+# install Werkzeug from pip3
 package { 'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'apt-get'
+  provider => 'pip3'
 }
