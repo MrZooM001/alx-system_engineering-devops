28f6d6692f23eaabc4758064147b7f496c83b835
Hazem Magdy <hhazem0010@gmail.com>
2024-01-26 20:18:07 +0200

    1st task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,31 +9,31 @@ package { 'nginx':
 }
 
 exec { 'allow HTTP':
-    command => 'ufw allow "Nginx HTTP"',
-    path    => '/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin',
+        command => 'ufw allow "Nginx HTTP"',
+        path    => '/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin',
 }
 
 exec { 'chmod www directory':
-    command => 'chmod -R 755 /var/www/',
-    path    => '/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin',
+        command => 'chmod -R 755 /var/www/',
+        path    => '/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin',
 }
 
 file { 'index.nginx-debian.html':
-    path    => '/var/www/html/index.nginx-debian.html',
-    content => 'Hello World!',
+        path    => '/var/www/html/index.nginx-debian.html',
+        content => 'Hello World!',
 }
 
 exec { 'config':
-    command  => 'sed -i "53i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
-    provider => 'shell',
+        command  => 'sed -i "53i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+        provider => 'shell',
 }
 
 service { 'nginx':
-        ensure => running,
-        require => Package['nginx']
+        ensure  => running,
+        require => Package['nginx'],
 }
 
 exec { 'restart':
-    command => 'sudo service nginx restart',
-    provider => 'shell',
+        command  => 'sudo service nginx restart',
+        provider => 'shell',
 }
