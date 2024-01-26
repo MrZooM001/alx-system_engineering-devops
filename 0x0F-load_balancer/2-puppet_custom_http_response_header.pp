440b68bb7b4268870ef5bc55ce6d5baae37b8c50
Hazem Magdy <hhazem0010@gmail.com>
2024-01-26 20:25:01 +0200

    task 3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,39 @@
 # Add a custom HTTP header with Puppet
 
-exec { 'update system':
+exec {'update system':
         command => '/usr/bin/apt-get update',
 }
 
-package { 'nginx':
+package {'nginx':
         ensure => 'installed',
 }
 
-exec { 'allow HTTP':
+exec {'allow HTTP':
         command => 'ufw allow "Nginx HTTP"',
         path    => '/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin',
 }
 
-exec { 'chmod www directory':
+exec {'chmod www directory':
         command => 'chmod -R 755 /var/www/',
         path    => '/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin',
 }
 
-file { 'index.nginx-debian.html':
+exec {'HTTP header':
+        command  => 'sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+        provider => 'shell'
+}
+
+file {'index.nginx-debian.html':
         path    => '/var/www/html/index.nginx-debian.html',
         content => 'Hello World!',
 }
 
-exec { 'config':
+exec {'config':
         command  => 'sed -i "53i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
         provider => 'shell',
 }
 
-service { 'nginx':
+service {'nginx':
         ensure  => running,
         require => Package['nginx'],
-}
-
-exec { 'restart':
-        command  => 'sudo service nginx restart',
-        provider => 'shell',
 }
