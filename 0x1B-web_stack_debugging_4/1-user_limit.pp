1f073415689223b1c1901ad751d6f356b425b35d
Hazem Magdy <hhazem0010@gmail.com>
2024-02-29 23:11:23 +0200

    fix task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Set OS configuration to login with holberton user
 exec {'OS security config':
-  command => 'sed -i "s/holberton/" /etc/security/limits.conf',
-  path    => '/bin/:/usr/bin/env/:/usr/bin/:/usr/sbin/'
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/:/usr/bin/env/:/bin/:/usr/sbin/'
 }
